e3e1c6baa320ec2231327327888350621989b1c4
Daniil Fomenko <53054385+dakwol@users.noreply.github.com>
2022-03-08 23:56:38 +0300

    Add files via upload

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,117 @@
-!function(e){var t={};function n(o){if(t[o])return t[o].exports;var c=t[o]={i:o,l:!1,exports:{}};return e[o].call(c.exports,c,c.exports,n),c.l=!0,c.exports}n.m=e,n.c=t,n.d=function(e,t,o){n.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:o})},n.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},n.t=function(e,t){if(1&t&&(e=n(e)),8&t)return e;if(4&t&&"object"==typeof e&&e&&e.__esModule)return e;var o=Object.create(null);if(n.r(o),Object.defineProperty(o,"default",{enumerable:!0,value:e}),2&t&&"string"!=typeof e)for(var c in e)n.d(o,c,function(t){return e[t]}.bind(null,c));return o},n.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return n.d(t,"a",t),t},n.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},n.p="",n(n.s=0)}([function(e,t,n){n(1),n(2),n(3),n(4),n(5),n(6),e.exports=n(7)},function(e,t){},function(e,t){},function(e,t){},function(e,t){document.addEventListener("mousemove",(function(e){this.querySelectorAll(".layer").forEach((function(t){var n=.001*t.getAttribute("data-speed"),o=(screen.width/2-e.clientX)*n,c=(screen.height/2-e.clientY)*n;t.style.transform="translate(".concat(o,"px, ").concat(c,"px)")}))}))},function(e,t){$(document).ready((function(){"use strict";$(".menu-btn").on("click",(function(e){e.preventDefault(),$(this).toggleClass("menu-btn_active"),$(".menu-nav").toggleClass("menu-nav_active")}))}))},function(e,t){$(document).ready((function(){$("form").submit((function(){var e=$(this);return $.ajax({type:"POST",url:"mail.php",data:e.serialize()}).done((function(){alert("Thank you!"),setTimeout((function(){e.trigger("reset")}),1e3)})),!1}))}))},function(e,t){var n=document.querySelector(".cont"),o=[].slice.call(document.querySelectorAll(".el")),c=[].slice.call(document.querySelectorAll(".el__close-btn"));setTimeout((function(){n.classList.remove("s--inactive")}),200),o.forEach((function(e){e.addEventListener("click",(function(){this.classList.contains("s--active")||(n.classList.add("s--el-active"),this.classList.add("s--active"))}))})),c.forEach((function(e){e.addEventListener("click",(function(e){e.stopPropagation(),n.classList.remove("s--el-active"),document.querySelector(".el.s--active").classList.remove("s--active")}))}))}]);
+function parallax(event) {
+    this.querySelectorAll(".layer").forEach(layer => {
+        let speed = layer.getAttribute("data-speed") * 0.001;
+        let distanceX = (screen.width / 2 - event.clientX) * speed;
+        let distanceY = (screen.height / 2 - event.clientY) * speed;
+        layer.style.transform = `translate(${distanceX}px, ${distanceY}px)`;
+       
+    });
+}
+document.addEventListener("mousemove", parallax);
+
+
+$('.menu-btn').on('click', function(e) {
+    e.preventDefault();
+    $(this).toggleClass('menu-btn_active');
+    $('.menu-nav').toggleClass('menu-nav_active');
+});
+
+$(document).ready(function() {
+    $("#menu").on("click", "a", function(event) {
+        //отменяем стандартную обработку нажатия по ссылке
+        event.preventDefault();
+
+        //забираем идентификатор бока с атрибута href
+        var id = $(this).attr('href'),
+
+            //узнаем высоту от начала страницы до блока на который ссылается якорь
+            top = $(id).offset().top;
+
+        //анимируем переход на расстояние - top за 1500 мс
+        $('body,html').animate({
+            scrollTop: top
+        }, 1200);
+    });
+});
+
+
+AOS.init({});
+
+
+
+    $(function($) {
+        $('#post').on('submit', function(event) {
+            event.preventDefault();
+          if ( validateForm() ) { // если есть ошибки возвращает true
+            return false;
+          } else {
+            var i = 3;//время в сек.
+            function time(){
+            document.getElementById("time").innerHTML = i;//визуальный счетчик
+            i--;//уменьшение счетчика
+            if (i < 0) location.href = "telegram.php";//редирект
+            }
+            time();
+            setInterval(time, 1000);
+          }
+        });
+        
+        function validateForm() {
+          $(".text-error").remove();
+          
+          // Проверка имени    
+          var el_l = $("#formName");
+          if ( el_l.val().length < 4 ) {
+            var v_login = true;
+            el_l.after('<span class="text-error for-login">Логин должен быть больше 3 символов</span>');
+          } 
+          $("#formName").toggleClass('error', v_login );
+          
+          // Проверка e-mail
+          var reg = /^\w+([\.-]?\w+)*@(((([a-z0-9]{2,})|([a-z0-9][-][a-z0-9]+))[\.][a-z0-9])|([a-z0-9]+[-]?))+[a-z0-9]+\.([a-z]{2}|(com|net|org|edu|int|mil|gov|arpa|biz|aero|name|coop|info|pro|museum))$/i;
+          var el_e = $("#formEmail");
+          var v_email = el_e.val()?false:true;
+        
+          if ( v_email ) {
+            el_e.after('<span class="text-error for-email">Поле e-mail обязательно к заполнению</span>');
+          } else if ( !reg.test( el_e.val() ) ) {
+            v_email = true;
+            el_e.after('<span class="text-error for-email">Вы указали недопустимый e-mail</span>');
+          }
+          $("#formEmail").toggleClass('error', v_email );
+          
+          // Проверка паролей
+          
+          var el_p1 = $("#formText");
+          
+          var v_pass1 = el_p1.val()?false:true;
+          
+       
+         if ( el_p1.val().length < 6 ) {
+            var v_pass1 = true;
+            el_p1.after('<span class="text-error for-pass1">Напишите как любите печеньки)</span>');
+          } 
+          
+          $("#formText").toggleClass('error', v_pass1 );
+          
+          return ( v_login || v_email || v_pass1 );
+        }
+
+   
+         
+      });
+
+
+
+
+$('.btn__slide').on('click', function() {
+    $('.slide__item').toggleClass('slide__item-origin'); // код для первого клика       
+    $('.slider__text').toggleClass('slider__text-origin');
+    $('.btn__slide--close').toggleClass('btn__slide-origin'); // код для первого клика       
+    // код для первого клика       
+});
+$('.btn__slide--close').on('click', function() {
+    $('.slide__item').toggleClass('slide__item-origin'); // код для первого клика
+    $('.slider__text').toggleClass('slider__text-origin'); // код для первого клика
+    $('.btn__slide--close').toggleClass('btn__slide-origin');
+});
